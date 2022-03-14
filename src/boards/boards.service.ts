ed617d2f8ce99f6197080edd20d6e131d985d1e8
nno3onn <nno3onn@naver.com>
2022-03-14 23:46:00 +0900

    CRUD board

---
+++ src/boards/boards.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Injectable()
+export class BoardsService {
+  private boards: Board[] = [];
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  createBoard(createBoardDto: CreateBoardDto): Board {
+    const { title, description } = createBoardDto;
+
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+    console.log(board);
+    this.boards.push(board);
+    return board;
+  }
+
+  getBoardById(id: string): Board {
+    const board = this.boards.find((board) => board.id === id);
+
+    if (!board) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+    return board;
+  }
+
+  deleteBoard(id: string): void {
+    const board = this.getBoardById(id);
+
+    if (!board) {
+      throw new NotFoundException();
+    }
+
+    this.boards = this.boards.filter((board) => board.id !== id);
+  }
+
+  updateBoardStatus(id: string, status: BoardStatus): Board {
+    const board = this.getBoardById(id);
+
+    if (!board) {
+      throw new NotFoundException();
+    }
+
+    board.status = status;
+    return board;
+  }
+}
