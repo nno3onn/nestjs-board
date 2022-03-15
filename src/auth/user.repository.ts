8b800e536309a01f5219342e6e14cc09189d3d61
nno3onn <nno3onn@naver.com>
2022-03-15 16:56:32 +0900

    encrypt password using bcrypt

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,12 +5,16 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
 
     // user Entity의 @Unique에서 이미 있는 이름을 저장하는 경우에 에러 발생 시, 에러 내용을 다르게 주기 위함
     try {
