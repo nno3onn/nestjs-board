32bf693acc28cd745b3ffb3d56e08fcbcade9155
nno3onn <nno3onn@naver.com>
2022-03-15 16:25:08 +0900

    signup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserRepository])],
   providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
