bb282ebb599a4eb5aaede792039ea8a9e5dffc93
nno3onn <nno3onn@naver.com>
2022-03-15 23:11:47 +0900

    allow only own posts to be deleted

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -74,8 +74,11 @@ export class BoardsController {
   // }
 
   @Delete('/:id')
-  deleteBoard(@Param('id', ParseIntPipe) id): Promise<void> {
-    return this.boardService.deleteBoard(id);
+  deleteBoard(
+    @Param('id', ParseIntPipe) id,
+    @GetUser() user: User,
+  ): Promise<void> {
+    return this.boardService.deleteBoard(id, user);
   }
 
   // @Patch('/:id/status')
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -83,8 +83,9 @@ export class BoardsService {
   // - remove: 무조건 존재하는 아이템만 삭제할 수 있음. 없으면 에러 발생
   //            (1) 아이템 유무 확인 (2) 아이템 삭제 => db에 두 번 접근하므로 비효율적임
   // - delete: 아이템이 존재하면 지우고, 존재하지 않으면 아무 영향이 없음
-  async deleteBoard(id: number): Promise<void> {
-    const result = await this.boardRepository.delete(id);
+  async deleteBoard(id: number, user: User): Promise<void> {
+    // 자신의 게시글인 경우에만 삭제 가능 (user 추가)
+    const result = await this.boardRepository.delete({ id, user });
     // 있는 아이템을 삭제했을 때: DeleteResult { raw: [], affected: 1 }
     // 없는 아이템을 삭제했을 때: DeleteResult { raw: [], affected: 0 }
 
