0278ded77ae3f92c0ef834f2b17f27f67fd13ca4
nno3onn <nno3onn@naver.com>
2022-03-15 15:29:17 +0900

    create auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './no-spec/auth/auth.controller';
 
 @Module({
   imports: [
     // 작성한 TypeORM 설정을 imports하여 TypeORM을 사용할 수 있게 함
     TypeOrmModule.forRoot(typeORMConfig),
     BoardsModule,
+    AuthModule,
   ],
+  controllers: [AuthController],
 })
 export class AppModule {}
